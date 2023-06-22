b2c109535d0faf97ccdadd69682e24802ab1abb0
Malinda Singh <malinda.singh@gmail.com>
2023-06-22 17:43:30 -0400

    setting up app

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,17 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  return (
+  <main>
+    <section className='container'>
+      <h3>0 BIRTHDAYS TODAY</h3>
+      <List />
+      <button onClick={()=> console.log('you clicked me')}>
+        CLEAR ALL
+      </button>
+    </section>
+    </main>
+  );
 }
 
 export default App;
